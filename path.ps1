76fd7161cd9943cde2223db78bb070765ad0b4d4
Tatu Lahtela <ext-tatu.lahtela@insta.fi>
2017-11-28 10:10:39 +0200

    Copy current path to clipboard

--- path.ps1
+++ path.ps1
@@ -4,4 +4,6 @@
 Copy current path into clipboard
 
 #>
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
 $pwd.Path | % { [Windows.Forms.Clipboard]::SetText($_)  }
