8bcddf117c22a2f6e421ba03b087c99c1dc4e2a9
Tatu Lahtela <lahtela@iki.fi>
2019-04-04 08:01:43 +0300

    silently continue on errors

--- f.ps1
+++ f.ps1
@@ -21,11 +21,11 @@ $editor = "C:\ProgramData\chocolatey\bin\emacsclient.exe"
 [string] $exclude = "(\.hg)"
 
 if ($open) {
-    $files = Get-ChildItem  -path . -recurse -filter "*$args*" | ? { $_.FullName -notmatch $exclude } | Select-Object -Property FullName -Expand FullName
+    $files = Get-ChildItem -ErrorAction SilentlyContinue -path . -recurse -filter "*$args*" | ? { $_.FullName -notmatch $exclude } | Select-Object -Property FullName -Expand FullName
     . $editor -n $files
 } elseif ($goto) {
-    Get-ChildItem  -path . -recurse -filter "*$args*" |  ? { $_.FullName -notmatch $exclude } | Select -First 1 -ExpandProperty DirectoryName | cd
+    Get-ChildItem -ErrorAction SilentlyContinue -path . -recurse -filter "*$args*" |  ? { $_.FullName -notmatch $exclude } | Select -First 1 -ExpandProperty DirectoryName | cd
 
 } else {
-    Get-ChildItem  -path . -recurse -filter "*$args*" |  ? { $_.FullName -notmatch $exclude } | Select-Object -Property FullName | Format-List *
+    Get-ChildItem -ErrorAction SilentlyContinue -path . -recurse -filter "*$args*" |  ? { $_.FullName -notmatch $exclude } | Select-Object -Property FullName | Format-List *
 }
